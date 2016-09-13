81d26243548df147d2c4a038c1a352f99be42ddd
Fran Fabrizio <fran@umn.edu>
2016-09-13 15:49:17 -0500

    Added YAML configuration and license info to project.

---
+++ json2redmine.rb
@@ -0,0 +1,256 @@
+# This file is part of the Minnesota Population Center's rt2redmine project.
+# For copyright and licensing information, see the NOTICE and LICENSE files
+# in this project's top-level directory, and also on-line at:
+#   https://github.com/mnpopcenter/rt2redmine
+
+require 'rest-client'
+require 'JSON'
+require 'active_resource'
+require 'activeresource-response'
+require 'pp'
+require 'mysql'
+require 'yaml'
+
+$config = (YAML.load_file('config/config.yml'))['json2redmine']
+
+# Capture global configuration for the ActiveResource objects
+
+class RestAPI < ActiveResource::Base 
+    
+  # overriding ActiveResource::Base's default handling of headers, because we need to set a different
+  # value for the X-Redmine-Switch-User header on each request, and after much testing, this is the
+  # way to do that
+  cattr_accessor :static_headers
+   self.static_headers = headers
+   
+   cattr_accessor :redmine_user
+
+   def self.headers
+     new_headers = static_headers.clone
+     if self.redmine_user
+       new_headers["X-Redmine-Switch-User"] = RestAPI.redmine_user # voila, evaluated at request-time
+     end
+     new_headers
+   end
+  # end header behavior modification
+   
+  # set our ActiveResource config
+  self.site = $config['url']
+  # a user's API key to create the Issues.  Will be set at the author so should
+  # probably use a service account.
+  self.user = $config['key']
+  # random string required for password when using API key as user
+  self.password = 'abcd'
+  
+  # this hack fixed a problem where I would get "Subject can't be blank" when trying
+  # to save the Issue
+  self.include_root_in_json = true   
+  
+  add_response_method :my_response
+  
+  # ACTIVATE for DEBUGGING OUTPUT (not too useful, but somewhat)
+  # RestAPI.logger = Logger.new(STDERR)
+end
+
+# subclass from RestAPI for Issues.  So far, this is the only one we need.
+# (thought we needed User but thus far not yet)
+class Issue < RestAPI; 
+  self.element_name = "issue" 
+end
+
+# we will need a Pg DB connection - see why later
+conn = Mysql.new($config['dbhost'], $config['dbuser'], $config['dbpass'], $config['database'])
+
+directories = Dir.entries($config['tickets_directory']).sort_by {|s| s.to_i } 
+directories.each() do |ticketdir|
+  next if ticketdir == '.' or ticketdir == '..'
+  # do work on real items
+  # skip if below start number
+  next if ticketdir.to_i < $config['start']
+  
+  jsonfile = $config['tickets_directory'] + ticketdir + '/' + ticketdir + '.json'
+  file = File.read(jsonfile)
+  ticket = JSON.parse(file)
+  #pp ticket 
+  # get RT ID
+  rt_id = /ticket\/(\d+)/.match(ticket['id'])[1]
+  
+  # Creating an issue
+  issue = Issue.new(
+    :subject => '[' + ticket['Requestors'] + " - RT \##{rt_id}" + '] ' + ticket['Subject'],
+    :project_id => $config['project_id'],
+    :description => (ticket['Description'] || ticket['Transactions'][0]['Content']),
+    # tracker 6 is request
+    :tracker_id => 6
+  )
+
+  RestAPI.redmine_user = false
+  # general pattern to follow when doing a .save - first try as the RT username (hoping
+  # there is an equivalent Redmine login), then
+  # fall back to the Redmine administrative account
+  begin
+    # attempt to impersonate user who created ticket
+    matcharray = (/(.*?)@umn.edu/i).match(ticket['Creator'])
+    username = matcharray[1] if matcharray
+    if username
+      puts "Trying to create issue as username #{username}"
+      RestAPI.redmine_user = username
+    end
+    if issue.save
+      puts "Created #{issue.id} from RT #{ticket['id']}"
+    else
+      puts issue.errors.full_messages
+    end
+  rescue ActiveResource::ClientError => msg
+    # revert to using admin as user 
+    RestAPI.redmine_user = false
+    if issue.save
+      puts "Could not create as user (#{msg.response.code}). Created #{issue.id} from RT #{ticket['id']} as redmine admin."
+    else
+      puts issue.errors.full_messages
+    end    
+  end 
+  
+  # set status
+  stat_id = 0
+  # determine status
+  if ticket['Status'] == 'resolved'
+    stat_id = $config['closed_status_id']
+  else 
+    stat_id = $config['open_status_id']
+  end
+  
+  # connect to the DB and manually reset the created_on date to the creation time of original ticket
+  # tried many ways to do this in a Rails-y way but nothing seems to work for Rails 4 as it did for Rails 3
+  time = DateTime.parse(ticket['Created'])
+  conn.query( "UPDATE issues set created_on = '#{time}', status_id = #{stat_id} where id = #{issue.id}" ) do |result|
+    if (result.result_status != 1) 
+      puts "ERROR: FAILED TO UPDATE created_on and status_id FOR ISSUE."
+    end
+  end
+  
+  # track most recent transaction for this issue
+  last_trans_date = time
+
+  # back to API usage
+  RestAPI.redmine_user = false
+  # add the ticket history
+  ticket['Transactions'].drop(1).each do |trans|
+    issue.notes = '[' + trans['Creator'] + '] ' + trans['Content']
+    
+    begin
+      # attempt to impersonate user who created transaction
+      matcharray = (/$config['username_regex']/i).match(trans['Creator'])
+      username = matcharray[1] if matcharray
+      if username   
+        puts "Trying to add transaction as username #{username}"
+        RestAPI.redmine_user = username
+      end
+      if res = issue.save
+        puts "Added #{trans['Type']} transaction to #{issue.id}"
+      else
+        puts issue.errors.full_messages
+      end
+    rescue ActiveResource::ClientError => msg
+      # revert to using admin as user 
+      RestAPI.redmine_user = false
+      if res = issue.save
+        puts "Could not add transaction as user (#{msg.response.code}). Added #{trans['Type']} transaction to #{issue.id} as redmine admin."
+      else
+        puts issue.errors.full_messages
+      end    
+    end
+    
+    # for each notes entry, alter its created_on date
+    # because ActiveResource sucks, I'm reverting to SQL here
+    # first, I need the journal ID that was assigned, this would be the latest journaled item (don't run this
+    # while others are using the system)
+    # I realize this is a terrible hack.  I'm too frustrated with ActiveResource to care.
+    trans_id = -1
+    conn.query("SELECT max(id) FROM journals") do |result|
+        trans_id = result.fetch_row[0]       
+    end
+    
+    # connect to the DB and manually reset the updated_on date to the time of transaction
+    # tried many ways to do this in a Rails-y way but nothing seems to work for Rails 4 as it did for Rails 3
+    time = DateTime.parse(trans['Created'])
+    conn.query( "UPDATE journals set created_on = '#{time}' where id = #{trans_id}" ) do |result|
+      if (result.result_status != 1)
+        puts "ERROR: FAILED TO UPDATE updated_on FOR TRANSACTION."
+      end
+    end 
+    last_trans_date = time
+  end
+  
+  # add the attachments, if any
+  # don't know how to do this via ActiveResource, going to do this with rest client
+  # attachments are all attached using the redmine admin API key.  I don't think this loses any useful info.
+    
+  if Dir.exists?($config['tickets_directory'] + ticketdir + '/attachments/')
+    directories = Dir.entries($config['tickets_directory'] + ticketdir + '/attachments/').sort_by {|s| s.to_i } 
+    directories.each do |file|
+      next if file == '.' or file == '..'
+      File.open($config['tickets_directory'] + ticketdir + '/attachments/' + file, 'rb') do |f|
+        puts "Processing attachment #{file}"
+        file_name = File.basename(f)
+        begin
+          # First we upload the image to get an attachment token
+          response = RestClient::Request.execute(:url => $config['upload_url'] + "?key=#{$config['key']}", :method => :post, :payload => f, :headers => {:multipart => true, :content_type => 'application/octet-stream'}, :verify_ssl => false)      
+        rescue RestClient::UnprocessableEntity => ue
+          p "The following exception typically means that the file size of '#{file_name}' exceeds the limit configured in Redmine."
+          raise ue
+        end
+        token = JSON.parse(response)['upload']['token']
+        id = issue.id
+        issue_url = $config['issue_url_base'] + "#{id}.json?key=#{$config['key']}"
+        response = RestClient::Request.execute(:url => issue_url, :method => :put, :verify_ssl => false, :payload => {
+          :attachments => {
+            :attachment1 => {
+              :token => token,
+              :filename => file_name,
+            }
+          } } )
+          
+          # update the file attachment transaction date
+          trans_id = -1
+          conn.query("SELECT max(id) FROM journals") do |result|
+              trans_id = result.fetch_row[0]       
+          end
+          # connect to the DB and manually reset the updated_on date to the time of transaction
+          # tried many ways to do this in a Rails-y way but nothing seems to work for Rails 4 as it did for Rails 3
+          # just use ticket creation time for attachments - close enough
+          time = DateTime.parse(ticket['Created'])
+          conn.query( "UPDATE journals set created_on = '#{time}' where id = #{trans_id}" ) do |result|
+            if (result.result_status != 1)
+              puts "ERROR: FAILED TO UPDATE created_on FOR ATTACHMENT."
+            end
+          end 
+          
+          # update the file uploaded date on attachments, same technique as above
+          trans_id = -1
+          conn.query("SELECT max(id) FROM attachments") do |result|
+              trans_id = result.fetch_row[0]       
+          end
+          # connect to the DB and manually reset the updated_on date to the time of transaction
+          # tried many ways to do this in a Rails-y way but nothing seems to work for Rails 4 as it did for Rails 3
+          # just use ticket creation time for attachments - close enough
+          time = DateTime.parse(ticket['Created'])
+          conn.query( "UPDATE attachments set created_on = '#{time}' where id = #{trans_id}" ) do |result|
+            if (result.result_status != 1)
+              puts "ERROR: FAILED TO UPDATE created_on FOR ATTACHMENT."
+            end
+          end 
+          
+      end
+    end
+  end
+  
+  # update ticket with date of last transaction
+  conn.query( "UPDATE issues set updated_on = '#{last_trans_date}' where id = #{issue.id}" ) do |result|
+    if (result.result_status != 1)
+      puts "ERROR: FAILED TO UPDATE updated_on for issue."
+    end
+  end 
+  
+  puts "----------------------------------------------"   
+end
---
+++ rt2json.pl
@@ -0,0 +1,240 @@
+# This file is part of the Minnesota Population Center's rt2redmine project.
+# For copyright and licensing information, see the NOTICE and LICENSE files
+# in this project's top-level directory, and also on-line at:
+#   https://github.com/mnpopcenter/rt2redmine
+
+use strict;
+
+use Error qw(:try);
+use RT::Client::REST;
+use MIME::Parser;
+use Data::Dumper;
+use JSON;
+use Getopt::Long;
+use File::Path qw/make_path/;
+use YAML qw/LoadFile/;
+
+# load config
+open my $fh, '<', 'config/config.yml' 
+  or die "can't open config file: $!";
+my $config = (LoadFile($fh))->{'rt2json'};
+
+# options handling
+
+my $start = $config->{'start'};
+my $debug = $config->{'debug'};
+GetOptions ("start=i" => \$start,    # numeric
+            "debug"  => \$debug)   # flag
+or die("Error in command line arguments\n");
+
+# real server
+my $rt = RT::Client::REST->new(server => $config->{'server'});
+
+# json coder
+my $json = new JSON->ascii->pretty;
+
+try {
+  # real server login
+  $rt->login(username => $config->{'username'}, password => $config->{'password'});
+} catch Exception::Class::Base with {
+  die "Can't login: ", shift->message;
+};
+
+my @ids;
+try {
+  @ids = $rt->search(type => "ticket", query => "Queue = '" . $config->{'queue'} . "' AND Id >= $start");
+} catch RT::Client::REST::UnauthorizedActionException with {
+  print "You are not authorized to view ticket\n";
+} catch RT::Client::REST::Exception with {
+  # something went wrong.
+};
+
+my $numtickets = scalar(@ids);
+my $processedtickets = 0;
+
+print "Starting loop to create JSON representations of tickets and save associated attachments.\n";
+foreach my $tid (sort {$a <=> $b} @ids) {
+  my $create_trans = undef;
+  print "Processing RT ticket $tid...\n";
+  my $ticket = $rt->show(type => 'ticket', id => $tid);
+  
+  # get history
+  my @trans_ids = $rt->get_transaction_ids(parent_id => $tid);
+  foreach my $trid (@trans_ids) {
+    my $trans = $rt->get_transaction(parent_id => $tid, id => $trid);
+
+    # we only care about the Create, Correspond and Comment transactions.  
+    # Other transaction types make no sense for Redmine (at least for ours).
+    if ($trans->{Type} =~ /Create|Correspond|Comment/) {
+      push @{$ticket->{Transactions}}, $trans;
+      # additionally, if this is the create transaction, grab ID so we can use later
+      if ($trans->{Type} =~ /Create/) {
+        $create_trans = $trid;
+      }
+    }
+  }
+  
+  # attachments
+  
+  # Ok, we have to process attachments first, because of the way RT processes incoming 
+  # emails / returns history in its API.  The content of the ticket request in the email is
+  # stored as a set of attachments (not just one becaue the email will be multipart/mixed, e.g. Gmail
+  # will send both an HTML and a plain text version, etc...)  I haven't looked closely at the RT code
+  # to determine how it decides which attachment to render in the web UI, but the net result from the API
+  # is that when you get the transaction history, the Content of the transaction object will usually not
+  # be the meaningful content you want (in my case, it was an email footer that was tacked on by mailing list
+  # software before the email hit RT).  To get to the valuable content, you have to process the attachments
+  # and extract the stuff you want.  Since I am migrating to Redmine, I want that initial content to be in
+  # the issue description.  RT doesn't have that concept of a ticket description - the request is just the first
+  # thing in the ticket history.  So we have to fudge a bit.
+  
+  my @att_ids = $rt->get_attachment_ids(id => $tid);
+  
+  if (@att_ids) {
+    push @{$ticket->{Attachment_IDs}}, @att_ids;
+  }
+  
+  # process attachments for this ticket, if any
+  # this means both "real" attachments, as in someone put a file on the ticket, and incoming emails, which get
+  # parsed into a series of attachments, and are not always reflected in the ticket history from the API like 
+  # you'd expect
+  my $current_parent;
+  my $alt_parent; 
+  foreach my $att_id (sort {$a <=> $b} @att_ids) {
+    # docs suggested we set undecoded to a TRUE value for binary data, but my experiements show the opposite.
+    # if you set undecoded to TRUE, you get Base64-encoded data out, which garbles the binary.
+    my $att = $rt->get_attachment(id => $att_id, parent_id => $tid, undecoded => 0);
+    print "Processing attachment $att_id ($att->{ContentType}, Parent: $att->{Parent})\n";
+       
+    # basic pattern is that an incoming email will have 1 or more 0b attachments as parents (multipart/mixed, or multipart/alternative content types, I believe) and then those will have multiple children (RT attachments have the concept of a parent attachment we can use to determine children) - basically, we MAY want to preserve text/plain children.  Some text/plain children are email footers - I believe at one time our incoming requests were routed through mailing list software before being forwarded to RT, and the mailing list added a footer - these can be distinguished from other text/plain children by looking at the Headers... for the footer attachments, the Headers had MIME information, whereas for the "real" content they did not seem to have that.
+         
+    if ($att->{ContentType} eq 'multipart/mixed') {
+      # we assume this is a parent attachment for an incoming email
+      # don't need to do anything but remember the ID
+      $current_parent = $att_id;
+      print "  Skipping - parent attachment. Set current_parent to $current_parent.\n";
+      next;
+    } elsif ($current_parent && $att->{Parent} == $current_parent && $att->{ContentType} eq 'multipart/alternative') {
+      $alt_parent = $att_id;
+      print "  Skipping - multipart/alternative child.  Set alt_parent to $alt_parent.\n";
+      next;
+    } elsif ($current_parent && ($att->{Parent} == $current_parent || $att->{Parent} == $alt_parent) && $att->{ContentType} eq 'text/html') {
+      print "  Skipping - alternative text/html child.\n";
+      next;
+    } elsif ($current_parent && ($att->{Parent} == $current_parent || $att->{Parent} == $alt_parent) && $att->{ContentType} eq 'text/plain') {
+      if ($att->{Headers} =~ /^MIME/) {
+        # this is likely a mailing list footer attachment; skip
+        print "  Skipping - extraneous footer.\n";
+        next;
+      } elsif ($att->{Transaction} == $create_trans) {
+        # This is the email content of the initial email that created the ticket.  We'll want to use
+        # it for the Redmine description, so tack it onto the ticket object itself.
+        print "  Special - text/plain child as part of the Create transaction.  Adding as ticket description.\n"; 
+        $ticket->{Description} = $att->{Content};
+        next;
+      } else {
+        # This is a text/plain update not done at creation time.  Could be a plain text file? 
+        print "  Continuing - Unknown text/plain child.\n" 
+      }
+    }
+    
+    # if you've gotten this far, I -think- the right thing to do is to only treat it as an attachment if a filename is present.
+    if (! $att->{Filename}) {
+      print "  Skipping - No filename.\n";
+      next;
+    }
+    
+    # we have a real attachment, write it out
+      print "  Yay! Real attachment. Saving.\n";    
+    make_path("tickets/$tid/attachments") if ! -d "tickets/$tid/attachments";
+    my $filename = "tickets/$tid/attachments/" . $att->{Filename};
+    open OUT, ">", $filename or die "Can't open $filename for writing.\n";
+    print OUT $att->{Content};
+    close OUT;
+    open OUT, ">", "tickets/$tid/att" . $att->{id} .  ".json" or die "Can't open tickets/$tid/att" . $att->{id} .  ".json for writing.\n";
+    print OUT $json->pretty->encode($att);
+    close OUT;
+    $debug && print Dumper($att);
+    
+    # verify length equivalent - this is a sanity check. 
+    # I'm not sure this is valid, though.
+    # sometimes there is no length in the headers - in this case we write out the attachment and hope for the best. 
+    my ($length) = $att->{Headers} =~ /Content-Length: (\d+)/;
+    # some files were off by 1 or 2 but were still open-able.  
+    if ($length && (abs($length - (-s $filename)) > 2)) {
+      warn "File lengths do not match! (ticket $tid, attachment $att_id, filename $filename, header length $length, fs length " . (-s $filename) . ")\n";
+    }
+  }
+  
+  # write out ticket data in JSON
+  mkdir "tickets/$tid" if ! -d "tickets/$tid";
+  open OUTJSON, ">", "tickets/$tid/$tid.json" or die "Can't open JSON file for output.\n";
+  print OUTJSON $json->pretty->encode($ticket);
+  close OUTJSON;
+  $debug && print $json->pretty->encode($ticket);
+  
+  print "done (" . ++$processedtickets . " of $numtickets)\n";
+}
+print "Finished loop to create JSON representations of tickets and save associated attachments.\n"
+
+
+ 
+#   my $historystr = `./webisoget-2.8.4/webisoget -text -formfile fran.login -url "https://rt.cla.umn.edu/REST/1.0/ticket/$tid/history?format=l"`;
+#   $historystr = strip_header(\$historystr);
+#   my @transactions = parse_history(\$historystr);
+#   print Dumper(\@transactions);
+
+# in the meantime, we'll do it with webisoget, clunky but works
+
+# tickets in admin queue
+# my $admin_tickets_REST = `./webisoget-2.8.4/webisoget -text -formfile fran.login -url "https://rt.cla.umn.edu/REST/1.0/search/ticket?query=Queue=%27mpc-admin-request%27"`;
+# my @lines = split /\n/, $admin_tickets_REST;
+# my @ticket_ids;
+# foreach $line (@lines) {
+#   my ($id) = $line =~ /^(\d+)\:/;
+#   push @ticket_ids, $id if $id;
+# }
+# print "Retrieved ticket IDs for mpc-admin-request queue.\n";
+#
+# # loop over tickets, getting properties and transaction history
+# foreach $tid (sort {$a <=> $b} @ticket_ids) {
+#   print "Processing $tid...\n";
+#   my $propstr = `./webisoget-2.8.4/webisoget -text -formfile fran.login -url "https://rt.cla.umn.edu/REST/1.0/ticket/$tid/show"`;
+#   $propstr = strip_header(\$propstr);
+#   my $props = parse_props(\$propstr);
+#   my $historystr = `./webisoget-2.8.4/webisoget -text -formfile fran.login -url "https://rt.cla.umn.edu/REST/1.0/ticket/$tid/history?format=l"`;
+#   $historystr = strip_header(\$historystr);
+#   my @transactions = parse_history(\$historystr);
+#   print Dumper(\@transactions);
+# }
+#
+# sub strip_header {
+#   my $strref = shift;
+#   $str = $$strref;
+#   $str =~ s/^.*?\n//;
+#   $str =~ s/^.*?\n//;
+#   return $str;
+# }
+#
+# sub parse_props {
+#   my $props = shift;
+#   open my $fh, '<', $props;
+#   my %prophash;
+#   while (<$fh>) {
+#     if (($field, $value) = /(.*?)\:\s+(.*?)\n/) {
+#       $prophash{$field} = $value;
+#     }
+#   }
+#   close $fh;
+#   return \$prophash;
+# }
+#
+# sub parse_history {
+#   my $history = shift;
+#   my @records = split /^--$/m, $$history;
+#   my $parser = new MIME::Parser;
+#   foreach my $rec (@records) {
+#     my $entity = $parser->parse_data(\$rec);
+#     print Dumper($entity);
+#   }
+# }
